d548710571c865b2d31dc96790092be79b8024c6
Ivan <mikheev.ivan91@gmail.com>
2019-07-22 12:33:21 +0300

    Создаем поле и заполняем его ячейками

--- MinesweeperGame.java
+++ MinesweeperGame.java
@@ -3,4 +3,21 @@ package com.javarush.games.minesweeper;
 import com.javarush.engine.cell.*;
 
 public class MinesweeperGame extends Game {
+    private static final int SIDE = 9;
+    private GameObject[][] gameField = new GameObject[SIDE][SIDE];
+
+    @Override
+    public void initialize() {
+        setScreenSize(SIDE, SIDE);
+        createGame();
+    }
+
+    private void createGame() {
+        for (int x = 0; x < gameField.length; x++) {
+            for (int y = 0; y < gameField.length; y++) {
+                gameField[y][x] = new GameObject(x, y);
+                setCellColor(x, y, Color.GRAY);
+            }
+        }
+    }
 }
