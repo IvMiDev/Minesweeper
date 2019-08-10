4dc2d322bd96d638ea85633bc281c421ba110113
Ivan <mikheev.ivan91@gmail.com>
2019-08-10 03:07:17 +0300

    Code refactoring and testing new features

--- MinesweeperGame.java
+++ MinesweeperGame.java
@@ -4,23 +4,18 @@ import com.javarush.engine.cell.*;
 import javax.swing.*;
 import java.util.ArrayList;
 
-public class MinesweeperGame extends Game implements Runnable {
+public class MinesweeperGame extends Game {
     private static final String MINE = "\uD83D\uDCA3";
     private static final String FLAG = "\uD83D\uDEA9";
+    private ImageIcon icon = new ImageIcon("C:\\Java\\bomb3.png");
     private GameObject[][] gameField;
-    private boolean isGameStopped, isNewGame, isFirstLaunch;
+    private boolean isGameStopped, isNewGame;
     private int side, countMinesOnField, countFlags, score, countClosedTiles;
 
     @Override
-    public void run() {
-        showFrame("New game!");
-    }
-
-    @Override
     public void initialize() {
-        isFirstLaunch = true;
         isNewGame = true;
-        run();
+        showFrame("New game!");
         synchronized (Thread.currentThread()) {
             try {
                 if (side == 0) {
@@ -29,15 +24,15 @@ public class MinesweeperGame extends Game implements Runnable {
             } catch (InterruptedException ignored) {
             }
         }
+        gameField = new GameObject[side][side];
+        setScreenSize(side, side);
+        createGame();
     }
 
     private void createGame() {
         isGameStopped = isNewGame = false;
-        countMinesOnField = 0;
-        countFlags = 0;
-        gameField = new GameObject[side][side];
+        countMinesOnField = countFlags = 0;
         countClosedTiles = side * side;
-        if (isFirstLaunch) setScreenSize(side, side);
         setScore(score = 0);
         for (int x = 0; x < gameField.length; x++) {
             for (int y = 0; y < gameField.length; y++) {
@@ -50,12 +45,29 @@ public class MinesweeperGame extends Game implements Runnable {
 
     private void setMines(int x, int y) {
         for (int i = 0; i < gameField.length; i++) {
-            for (int j = 0; j < gameField.length; j++) {
-                if (!(gameField[j][i].getX() == x && gameField[j][i].getY() == y)) {
-                    if (getRandomNumber(10) == 1) {
-                        gameField[j][i].setMine();
-                        countMinesOnField++;
-                        countFlags++;
+            for (GameObject[] gameObject : gameField) {
+                if (!(gameObject[i].getX() == x && gameObject[i].getY() == y)) {
+                    switch (side) {
+                        case 10:
+                            if (getRandomNumber(10) == 1) {
+                                gameObject[i].setMine();
+                                countMinesOnField++;
+                                countFlags++;
+                            }
+                            break;
+                        case 15:
+                            if (getRandomNumber(7) == 1) {
+                                gameObject[i].setMine();
+                                countMinesOnField++;
+                                countFlags++;
+                            }
+                            break;
+                        case 20:
+                            if (getRandomNumber(4) == 1) {
+                                gameObject[i].setMine();
+                                countMinesOnField++;
+                                countFlags++;
+                            }
                     }
                 }
             }
@@ -80,13 +92,13 @@ public class MinesweeperGame extends Game implements Runnable {
 
     private void countMineNeighbors() {
         for (int x = 0; x < gameField.length; x++) {
-            for (int y = 0; y < gameField.length; y++) {
-                if (!gameField[y][x].isMine()) {
-                    ArrayList<GameObject> mineNeighbors = new ArrayList<>(getNeighbors(gameField[y][x]));
+            for (GameObject[] gameObject : gameField) {
+                if (!gameObject[x].isMine()) {
+                    ArrayList<GameObject> mineNeighbors = new ArrayList<>(getNeighbors(gameObject[x]));
                     for (GameObject neighbor : mineNeighbors) {
                         if (neighbor.isMine()) {
-                            int countMineNeighbor = gameField[y][x].getCountMineNeighbors();
-                            gameField[y][x].setCountMineNeighbors(++countMineNeighbor);
+                            int countMineNeighbor = gameObject[x].getCountMineNeighbors();
+                            gameObject[x].setCountMineNeighbors(++countMineNeighbor);
                         }
                     }
                 }
@@ -102,9 +114,7 @@ public class MinesweeperGame extends Game implements Runnable {
             gameField[y][x].setOpen();
             if (!gameField[y][x].isMine()) {
                 setScore(score += 5);
-                countClosedTiles--;
-                if (countClosedTiles == countMinesOnField) {
-                    isFirstLaunch = false;
+                if (--countClosedTiles == countMinesOnField) {
                     isGameStopped = true;
                     showFrame("Congratulations! You won!!!");
                 }
@@ -138,7 +148,6 @@ public class MinesweeperGame extends Game implements Runnable {
                 }
             } else {
                 setCellValueEx(x, y, Color.RED, MINE);
-                isFirstLaunch = false;
                 isGameStopped = true;
                 showFrame("Game Over!");
             }
@@ -173,13 +182,7 @@ public class MinesweeperGame extends Game implements Runnable {
         }
     }
 
-    /*
-    All code below for test!!!
-    */
-
     private void showFrame(String title) {
-        String difficulty = "Choose difficulty level: ";
-        String restart = "Would you like to replay? ";
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
         JLabel label = new JLabel();
@@ -189,7 +192,7 @@ public class MinesweeperGame extends Game implements Runnable {
         panel.add(label);
         if (isNewGame) {
             frame.setTitle(title);
-            label.setText(difficulty);
+            label.setText("Choose difficulty level: ");
             b1.setText("Easy");
             b2.setText("Normal");
             b3.setText("Hard");
@@ -198,55 +201,47 @@ public class MinesweeperGame extends Game implements Runnable {
             panel.add(b3);
         } else {
             frame.setTitle(title);
-            label.setText(restart);
+            label.setText("Would you like to replay? ");
             b1.setText("Yes");
             b2.setText("No");
             panel.add(b1);
             panel.add(b2);
         }
         frame.add(panel);
-        frame.setSize(400, 200);
+        frame.setSize(400, 80);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setResizable(false);
         frame.setAlwaysOnTop(true);
+        frame.setIconImage(icon.getImage());
         frame.setVisible(true);
         Thread t = Thread.currentThread();
-        b1.addActionListener(e -> {
+        b1.addActionListener(event -> {
             if (isNewGame) {
                 side = 10;
                 frame.setVisible(false);
-                createGame();
-                if (isFirstLaunch) {
-                    synchronized (t) {
-                        t.notify();
-                    }
-                }
-            } else {
-                isNewGame = true;
-                frame.setVisible(false);
-                showFrame("New game!");
-            }
-        });
-        b2.addActionListener(e -> {
-            if (isNewGame) {
-                side = 15;
-                frame.setVisible(false);
-                createGame();
-                if (isFirstLaunch) {
-                    synchronized (t) {
-                        t.notify();
-                    }
-                }
-            } else System.exit(0);
-        });
-        b3.addActionListener(e -> {
-            side = 20;
-            frame.setVisible(false);
-            createGame();
-            if (isFirstLaunch) {
                 synchronized (t) {
                     t.notify();
                 }
+            } else {
+                frame.setVisible(false);
+                createGame();
+            }
+        });
+        b2.addActionListener(event -> {
+            if (isNewGame) {
+                side = 15;
+                frame.setVisible(false);
+                synchronized (t) {
+                    t.notify();
+                }
+            } else System.exit(0);
+        });
+        b3.addActionListener(event -> {
+            side = 20;
+            frame.setVisible(false);
+            synchronized (t) {
+                t.notify();
             }
         });
     }
